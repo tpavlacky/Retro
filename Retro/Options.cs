f540f60f7deba81174f203269ba33f666aa3e5b1
Kucera, Marek <mrk.kucera@gmail.com>
2021-01-21 14:43:48 +0100

    Changed desciption option to not require the -d switch, description is instead taken as the last argument

--- Retro/Options.cs
+++ Retro/Options.cs
@@ -17,7 +17,7 @@ namespace Retro
     [Option('t', "To", HelpText = "Kudos to who?")]
     public string KudosTarget { get; set; }
 
-    [Option('d', "Description", HelpText = "Description", Required = true)]
+    [Value(0, HelpText = "Description of the retro point", MetaName = "Description")]
     public string Description { get; set; }
   }
 
